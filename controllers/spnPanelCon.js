a2ec9f38d4c2f4dbafefa7f50eef8af243262483
Neda Najafi <n_unknown57@yahoo.com>
2022-05-29 22:44:07 +0430

    Update spnPanelCon.js

--- controllers/spnPanelCon.js
+++ controllers/spnPanelCon.js
@@ -3,7 +3,6 @@ const { CACHE } = require('./../config/keys');
 const fs = require('fs');
 
 
-
 exports.indexSpn = (req, res, next) => {
 
     res.sendFile(path.dirname(require.main.filename) + '/statics/indexSpn.html');
@@ -14,16 +13,10 @@ exports.createPanel = (req, res, next) => {
 }
 
 exports.fillData = async (req, res, next) => {
-
-    // let page = req.body.page ?? 0;
-    // let limit = req.body.limit ?? 10;
-    // let starteIndex = (page - 1) * limit;
-    // let endIndex = page * limit;
-
     let data = CACHE.get('config');
-
     return res.status(200).json(data || []);
 }
+
 exports.get = async (req, res, next) => {
 
     let data_ = CACHE.get('config');
@@ -33,10 +26,6 @@ exports.get = async (req, res, next) => {
     }
 
     let data = await data_.find(pack => pack.name === req.params.id);
-
-    // if(data.dependencies){
-    //     data.dependencies = Object.keys(data.dependencies);
-    // }
 
     let depToMe = await data_.filter(pack => pack.dependencies && pack.dependencies.includes(data.name));
     data.depToMe = [];
@@ -54,78 +43,82 @@ exports.chooseFile = async (req, res, next) => {
 
 exports.loadFile = async (req, res, next) => {
 
-    if (req.fileValidationError) {
-        return res.status(400).json('Ivalid input type');
-    }
-
-    await fs.readFile(path.dirname(require.main.filename) + `/statics/${req.file.filename}`, 'utf8', async (err, data) => {
-
-        let arr = data.split(/\n\s*\n/)
-        arr = arr.filter(n => n);
-
-        let packages = [];
-        await Promise.all(arr.map(pack => {
-            pack = pack.split('\r\n').filter(n => n)
-            pack = pack.filter(pp => pp !== '[[package]]' && !pp.includes('version'));
-
-            let json_ = {}
-            let extra_json = {}
-            let dependencies_json = []
-            for (p of pack) {
-                if (p === '[package.extras]') {
-                    for (let i = 1; i < pack.length; i++) {
-                        let j = pack[i].split(' = ');
-                        extra_json[j[0]] = j[1];
+    try{
+        if (req.fileValidationError) {
+            return res.status(400).json('Ivalid input type');
+        }
+    
+        await fs.readFile(path.dirname(require.main.filename) + `/statics/${req.file.filename}`, 'utf8', async (err, data) => {
+    
+            let arr = data.split(/\n\s*\n/)
+            arr = arr.filter(n => n);
+    
+            let packages = [];
+            await Promise.all(arr.map(pack => {
+                pack = pack.split('\r\n').filter(n => n)
+                pack = pack.filter(pp => pp !== '[[package]]' && !pp.includes('version'));
+    
+                let json_ = {}
+                let extra_json = {}
+                let dependencies_json = []
+                for (p of pack) {
+                    if (p === '[package.extras]') {
+                        for (let i = 1; i < pack.length; i++) {
+                            let j = pack[i].split(' = ');
+                            extra_json[j[0]] = j[1];
+                        }
+    
+                        break;
+    
+                    } else if (p === '[package.dependencies]') {
+    
+                        for (let i = 1; i < pack.length; i++) {
+                            let j = pack[i].split(' = ');
+                            if (j[0]) {
+                                dependencies_json.push(j[0].replace(/ /g, ""));
+                            }
+                        }
+    
+                        break;
                     }
-
-                    break;
-
-                } else if (p === '[package.dependencies]') {
-
-                    for (let i = 1; i < pack.length; i++) {
-                        let j = pack[i].split(' = ');
-                        if (j[0]) {
-                            dependencies_json.push(j[0].replace(/ /g, ""));
+    
+                    let j = p.split('=');
+                    if (j[0] && j[1]) {
+                        j[1] = j[1].replace(/"/g, "");
+                        j[0] = j[0].replace(/ /g, "");
+                        if (j[0] === 'description') {
+                            json_[j[0]] = j[1];
+                        } else {
+                            json_[j[0]] = j[1].replace(/ /g, "");
                         }
                     }
-
-                    break;
+                };
+    
+              /*if(Object.keys(extra_json).length > 0){
+                packages[packages.length - 1].extra = extra_json;
+                extra_json = {};
+              }else */if (Object.keys(dependencies_json).length > 0) {
+                    packages[packages.length - 1].dependencies = dependencies_json;
+                    dependencies_json = {};
+                } else {
+                    packages.push(json_)
                 }
+    
+            }));
+    
+            packages = await packages.filter(n => Object.keys(n).length > 0)
+            packages.sort((a, b) => a.name < b.name ? -1 : 1);
+            packages.splice(packages.length - 2, 2)
+            CACHE.set('config', packages);
+            
+            fs.unlink(path.dirname(require.main.filename) + `/statics/${req.file.filename}`, (err) => {
+                console.log(err)
+            });
 
-                let j = p.split('=');
-                if (j[0] && j[1]) {
-                    j[1] = j[1].replace(/"/g, "");
-                    j[0] = j[0].replace(/ /g, "");
-                    if (j[0] === 'description') {
-                        json_[j[0]] = j[1];
-                    } else {
-                        json_[j[0]] = j[1].replace(/ /g, "");
-                    }
-                }
-            };
-
-          /*if(Object.keys(extra_json).length > 0){
-            packages[packages.length - 1].extra = extra_json;
-            extra_json = {};
-          }else */if (Object.keys(dependencies_json).length > 0) {
-                packages[packages.length - 1].dependencies = dependencies_json;
-                dependencies_json = {};
-            } else {
-                packages.push(json_)
-            }
-
-        }));
-
-        packages = await packages.filter(n => Object.keys(n).length > 0)
-        packages.sort((a, b) => a.name < b.name ? -1 : 1);
-        packages.splice(packages.length - 2, 2)
-        CACHE.set('config', packages);
-        res.end();
-        fs.unlink(path.dirname(require.main.filename) + `/statics/${req.file.filename}`, (err) => {
-            console.log(err)
+            res.status(200).end();
         });
-    });
-
-
-
+    }catch(err){
+        return res.status(400).json('Server error');
+    }
+    
 }
